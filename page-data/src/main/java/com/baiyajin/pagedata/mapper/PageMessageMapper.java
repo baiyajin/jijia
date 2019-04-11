d310899856a19baf36807f681e41e3cfd2cd652a
baiyajin <baiyajin110@qq.com>
2019-04-11 12:16:44 +0800

    BAI-7 更新消息

---
+++ page-data/src/main/java/com/baiyajin/pagedata/mapper/PageMessageMapper.java
@@ -0,0 +1,8 @@
+package com.baiyajin.pagedata.mapper;
+
+import com.baiyajin.pagedata.entity.PageMessage;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+public interface PageMessageMapper extends BaseMapper<PageMessage> {
+
+}
