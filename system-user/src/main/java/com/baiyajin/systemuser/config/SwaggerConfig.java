f0391b23ba6450a54bd13c047c1e16a64bc05c07
baiyajin <baiyajin110@qq.com>
2019-04-15 09:17:34 +0800

    Merge branch 'dev_wzx' of https://github.com/250747293/jijia
    # Conflicts:
    #	page-data/src/main/java/com/baiyajin/pagedata/controller/PageMaterialController.java
    #	page-data/src/main/java/com/baiyajin/pagedata/controller/PageMessageController.java
    #	page-data/src/main/java/com/baiyajin/pagedata/entity/PageMessage.java
    #	page-data/src/main/java/com/baiyajin/pagedata/mapper/PageMessageMapper.java
    #	page-data/src/main/java/com/baiyajin/pagedata/service/PageMessageInterface.java
    #	page-data/src/main/java/com/baiyajin/pagedata/service/PageMessageService.java
    #	page-data/src/main/java/com/baiyajin/pagedata/util
    #	page-data/src/main/resources/application.yml
    #	page-data/src/main/resources/mapper/PageMessageXML.xml

---
+++ system-user/src/main/java/com/baiyajin/systemuser/config/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.baiyajin.systemuser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author xiaqing
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.baiyajin.systemuser.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("后台用户管理接口")
+                .description("test")
+                .version("1.0")
+                .build();
+    }
+
+    /**
+     * 防止@EnableMvc把默认的静态资源路径覆盖了，手动设置的方式
+     *
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 解决静态资源无法访问
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+        // 解决swagger无法访问
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        // 解决swagger的js文件无法访问
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        /*将templates映射出去*/
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+
+    }
+
+}
